a0037e86d1da0a2e8a83d9bfcf2bbd559a1ec7dc
Orange <635000035@qq.com>
2020-07-23 10:56:29 +0800

    页面及功能调整

--- src/pages/taskManage/task.vue
+++ src/pages/taskManage/task.vue
@@ -142,7 +142,8 @@
           <div></div>
           <div>
             <el-pagination @size-change="handleSizeChange" @current-change="handleCurrentChange" :current-page="currentPage"
-              :page-sizes="[10, 20, 50, 100]" :page-size="10" layout="total, sizes, prev, pager, next, jumper" :total="total">
+              :page-sizes="[10, 20, 50, 100, 1000, 10000]" :page-size="10" layout="total, sizes, prev, pager, next, jumper"
+              :total="total">
             </el-pagination>
           </div>
         </div>
